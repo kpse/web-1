985c7658c67ca525eba73cf80f1111e2ee4f10e3
Suo Qin <kpsesuo@gmail.com>
2014-03-29 02:27:49 +0800

    add js version for cache bursting

---
+++ app/controllers/VersionedAssets.scala
@@ -0,0 +1,40 @@
+package controllers
+
+import play.api.mvc.PathBindable
+import play.api.Play
+import Play.current
+import java.io.File
+import play.Logger
+
+object VersionedAssets {
+  def at(file: VersionedAsset) = Assets.at(file.path, file.file)
+}
+
+case class VersionedAsset(file: String, path: String = "/public", versionParam: String = "v")
+
+object VersionedAsset {
+
+  implicit def pathBinder = new PathBindable[VersionedAsset] {
+    def bind(key: String, value: String) = {
+      Right(VersionedAsset(value))
+    }
+
+    def unbind(key: String, value: VersionedAsset) = {
+      val resourceName = Option(value.path + "/" + value.file).map(name => if (name.startsWith("/")) name else ("/" + name)).get
+      Logger.info("unbind: " + resourceName)
+      val modified = Play.resource(resourceName).flatMap {
+        resource =>
+          resource.getProtocol match {
+            case file if file == "file" => {
+              Logger.info(new File(resource.toURI).lastModified().toString)
+              Some(new File(resource.toURI).lastModified())
+            }
+            case _ => None
+          }
+      }
+      modified.map(value.file + "?" + value.versionParam + "=" + _).getOrElse(value.file)
+    }
+  }
+
+  implicit def toVersionedAsset(path: String): VersionedAsset = VersionedAsset(path)
+}
