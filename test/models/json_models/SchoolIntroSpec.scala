5db90afd992466515dc17ec8f27b8fa2c06c6ece
Suo Qin <kpsesuo@gmail.com>
2014-03-30 22:58:28 +0800

    add error message for school names length limitation

--- test/models/json_models/SchoolIntroSpec.scala
+++ test/models/json_models/SchoolIntroSpec.scala
@@ -12,10 +12,10 @@ class SchoolIntroSpec extends Specification with TestSupport {
       private val index = SchoolIntro.index
 
       index.size must equalTo(3)
-      index(0).name must equalTo("成都市第三军区幼儿园")
+      index(0).name must equalTo("第三军区幼儿园")
       index(0).school_id must equalTo(93740362L)
 
-      index(1).name must equalTo("西安市高新一幼")
+      index(1).name must equalTo("高新一幼")
       index(1).school_id must equalTo(93740562L)
 
     }
