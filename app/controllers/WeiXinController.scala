f9eba88353cb5879ebc8c1ae88fa57bd8b1a33e9
Suo Qin <kpsesuo@gmail.com>
2014-05-14 22:11:47 +0800

    add weixin message and clicking handler

--- app/controllers/WeiXinController.scala
+++ app/controllers/WeiXinController.scala
@@ -2,7 +2,13 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 import play.Logger
-import scala.xml.PCData
+import play.api.libs.json.{Json, JsValue, JsObject}
+import play.api.libs.ws.WS
+import play.api.Play
+import play.cache.Cache
+import scala.concurrent.{ExecutionContext, Promise, Future, Await}
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
 
 object WeiXinController extends Controller {
 
@@ -16,7 +22,8 @@ object WeiXinController extends Controller {
   def handleMessage(seq: NodeSeq) = {
     val from = seq \ "FromUserName"
     val to = seq \ "ToUserName"
-    newMessage(PCData(from.text), PCData(to.text), PCData("你好，%s".format(to.text)))
+    val userInfo = Await.result(Await.result(queryUserInfo(to.text), 5 second), 5 seconds)
+    newMessage(PCData(from.text), PCData(to.text), PCData("你好，%s".format((userInfo \ "nickname").as[String])))
   }
 
   def newMessage(from: PCData, to: PCData, content: PCData) = <xml>
@@ -40,7 +47,8 @@ object WeiXinController extends Controller {
   def handleClicking(seq: NodeSeq) = {
     val from = seq \ "FromUserName"
     val to = seq \ "ToUserName"
-    newMessage(PCData(from.text), PCData(from.text), PCData("谢谢点击，%s".format(to.text)))
+    val userInfo = Await.result(Await.result(queryUserInfo(to.text), 5 second), 5 seconds)
+    newMessage(PCData(from.text), PCData(to.text), PCData("谢谢点击，%s".format((userInfo \ "nickname").as[String])))
   }
 
   def handle = Action(parse.xml) {
@@ -55,6 +63,39 @@ object WeiXinController extends Controller {
           Ok("")
       }
 
+  }
+
+  def getToken(): Future[String] = {
+
+    Cache.get("weixin_token") match {
+      case token: String if !token.isEmpty =>
+        val promise = Promise[String]()
+        promise.success(token)
+        promise.future
+      case _ =>
+        val ak = Play.current.configuration.getString("weixin.ak").getOrElse("")
+        val sk = Play.current.configuration.getString("weixin.sk").getOrElse("")
+        val url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s".format(ak, sk)
+        Logger.info(url)
+        WS.url(url).get().map {
+          response =>
+            val token: String = (response.json \ "access_token").as[String]
+            Cache.set("weixin_token", token, 7200)
+            token
+        }
+    }
+  }
+
+  def queryUserInfo(openId: String) = {
+    getToken().map {
+      token =>
+        val url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s".format(token, openId)
+        Logger.info(url)
+        WS.url(url).get().map {
+          response =>
+            response.json
+        }
+    }
   }
 
 }
