cd3b000dd7ed3164780bdeeb16cc122b9548e832
Suo Qin <kpsesuo@gmail.com>
2014-05-15 11:12:48 +0800

    add v1001_intro and v1001_good handler

--- app/controllers/WeiXinController.scala
+++ app/controllers/WeiXinController.scala
@@ -22,17 +22,17 @@ object WeiXinController extends Controller {
   def handleMessage(seq: NodeSeq) = {
     val from = seq \ "FromUserName"
     val to = seq \ "ToUserName"
-//    val userInfo = Await.result(Await.result(queryUserInfo(to.text), 5 second), 5 seconds)
-//    newMessage(PCData(from.text), PCData(to.text), PCData("你好，%s".format((userInfo \ "nickname").as[String])))
-    newMessage(PCData(from.text), PCData(to.text), PCData("用户你好。"))
+    //    val userInfo = Await.result(Await.result(queryUserInfo(to.text), 5 second), 5 seconds)
+    //    newMessage(PCData(from.text), PCData(to.text), PCData("你好，%s".format((userInfo \ "nickname").as[String])))
+    newMessage(PCData(to.text), PCData(from.text), PCData("用户你好。"))
   }
 
   def newMessage(from: PCData, to: PCData, content: PCData) = <xml>
     <ToUserName>
-      {from}
+      {to}
     </ToUserName>
     <FromUserName>
-      {to}
+      {from}
     </FromUserName>
     <CreateTime>
       {System.currentTimeMillis}
@@ -45,12 +45,75 @@ object WeiXinController extends Controller {
     </Content>
   </xml>;
 
+  def IntroMessage(from: PCData, to: PCData) = <xml>
+    <ToUserName>
+      {to}
+    </ToUserName>
+    <FromUserName>
+      {from}
+    </FromUserName>
+    <CreateTime>
+      {System.currentTimeMillis}
+    </CreateTime>
+    <MsgType>
+      {PCData("news")}
+    </MsgType>
+    <ArticleCount>2</ArticleCount>
+    <Articles>
+      <item>
+        <Title>
+          {PCData("我们是幼乐宝")}
+        </Title>
+        <Description>
+          {PCData("成都库乐宝信息技术有限公司位于成都市高新区天府软件园旁，是一家专业从事幼教行业移动互联网应用的高新技术企业，拥有多项自主知识产权及产品，行业经验及技术能力均处于国内领先地位。")}
+        </Description>
+        <PicUrl>
+          {PCData("http://kulebao-prod.qiniudn.com/11.jpg")}
+        </PicUrl>
+        <Url>
+          {PCData("http://www.cocobabys.com")}
+        </Url>
+      </item>
+      <item>
+        <Title>
+          {PCData("幼乐宝的重要性")}
+        </Title>
+        <Description>
+          {PCData(
+            """
+              |对于家长：
+              |将孩子送到幼儿园，宝宝日常的衣食住行都是每位父母担心的问题。
+              |宝宝的安全更是为人父母关注的重点，宝宝的安全出入园信息如何有效反馈。
+              |宝宝入园之后会不会被别有用心的人冒名领走？年迈的爷爷奶奶有没有及时接送宝宝？
+              |宝宝每日在幼儿园开不开心、安不安全？一大堆的问题都是家长希望及时知道的。
+              |原来我们只能采用听老师口述、收发短信、拨打电话等费时费力高成本的沟通方式，既不能满足现代家长快节奏工作的需要，又浪费了大量金钱和时间。
+              |现在，家长们只需要动动手指就能解决这一切问题。
+            """.stripMargin)}
+        </Description>
+        <PicUrl>
+          {PCData("http://kulebao-prod.qiniudn.com/logo_200.png")}
+        </PicUrl>
+        <Url>
+          {PCData("http://www.cocobabys.com/app_package/download")}
+        </Url>
+      </item>
+    </Articles>
+  </xml>;
+
   def handleClicking(seq: NodeSeq) = {
     val from = seq \ "FromUserName"
     val to = seq \ "ToUserName"
-//    val userInfo = Await.result(Await.result(queryUserInfo(to.text), 5 second), 5 seconds)
-//    newMessage(PCData(from.text), PCData(to.text), PCData("谢谢点击，%s".format((userInfo \ "nickname").as[String])))
-    newMessage(PCData(from.text), PCData(to.text), PCData("谢谢您的点击测试。"))
+    //    val userInfo = Await.result(Await.result(queryUserInfo(to.text), 5 second), 5 seconds)
+    //    newMessage(PCData(from.text), PCData(to.text), PCData("谢谢点击，%s".format((userInfo \ "nickname").as[String])))
+    val key = (seq \ "EventKey").text
+    key match {
+      case "V1001_INTRO" =>
+        IntroMessage(PCData(from.text), PCData(to.text))
+      case "V1001_GOOD" =>
+        newMessage(PCData(to.text), PCData(from.text), PCData("谢谢你的赞，我们会继续努力的。"))
+      case _ =>
+        newMessage(PCData(to.text), PCData(from.text), PCData("谢谢您的点击测试。"))
+    }
   }
 
   def handle = Action(parse.xml) {
@@ -67,7 +130,7 @@ object WeiXinController extends Controller {
 
   }
 
-  def getToken(): Future[String] = {
+  def getToken: Future[String] = {
 
     Cache.get("weixin_token") match {
       case token: String if !token.isEmpty =>
@@ -89,7 +152,7 @@ object WeiXinController extends Controller {
   }
 
   def queryUserInfo(openId: String) = {
-    getToken().map {
+    getToken.map {
       token =>
         val url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=%s&openid=%s".format(token, openId)
         Logger.info(url)
