bda9733b9901662f03258b15bf121924f1ab3444
Suo Qin <kpsesuo@gmail.com>
2014-05-15 21:56:48 +0800

    add subscribe event handler

--- app/controllers/WeiXinController.scala
+++ app/controllers/WeiXinController.scala
@@ -80,15 +80,15 @@ object WeiXinController extends Controller {
         </Title>
         <Description>
           {PCData(
-            """
-              |对于家长：
-              |将孩子送到幼儿园，宝宝日常的衣食住行都是每位父母担心的问题。
-              |宝宝的安全更是为人父母关注的重点，宝宝的安全出入园信息如何有效反馈。
-              |宝宝入园之后会不会被别有用心的人冒名领走？年迈的爷爷奶奶有没有及时接送宝宝？
-              |宝宝每日在幼儿园开不开心、安不安全？一大堆的问题都是家长希望及时知道的。
-              |原来我们只能采用听老师口述、收发短信、拨打电话等费时费力高成本的沟通方式，既不能满足现代家长快节奏工作的需要，又浪费了大量金钱和时间。
-              |现在，家长们只需要动动手指就能解决这一切问题。
-            """.stripMargin)}
+          """
+            |对于家长：
+            |将孩子送到幼儿园，宝宝日常的衣食住行都是每位父母担心的问题。
+            |宝宝的安全更是为人父母关注的重点，宝宝的安全出入园信息如何有效反馈。
+            |宝宝入园之后会不会被别有用心的人冒名领走？年迈的爷爷奶奶有没有及时接送宝宝？
+            |宝宝每日在幼儿园开不开心、安不安全？一大堆的问题都是家长希望及时知道的。
+            |原来我们只能采用听老师口述、收发短信、拨打电话等费时费力高成本的沟通方式，既不能满足现代家长快节奏工作的需要，又浪费了大量金钱和时间。
+            |现在，家长们只需要动动手指就能解决这一切问题。
+          """.stripMargin)}
         </Description>
         <PicUrl>
           {PCData("http://kulebao-prod.qiniudn.com/logo_200.png")}
@@ -116,14 +116,22 @@ object WeiXinController extends Controller {
     }
   }
 
+  def handleSubscribe(seq: NodeSeq) = {
+    val from = seq \ "FromUserName"
+    val to = seq \ "ToUserName"
+    newMessage(PCData(to.text), PCData(from.text), PCData("谢谢您关注库乐宝公司公司账号，我们的产品地址是https://cocobabys.com"))
+  }
+
   def handle = Action(parse.xml) {
     request =>
       Logger.info(request.body.toString())
       request.body match {
-        case message if (request.body \ "MsgType").text.equals("text") =>
-          Ok(handleMessage(request.body))
-        case clicking if (request.body \ "MsgType").text.equals("event") =>
-          Ok(handleClicking(request.body))
+        case message if (message \ "MsgType").text.equals("text") =>
+          Ok(handleMessage(message))
+        case subscribe if (subscribe \ "MsgType").text.equals("event") && (subscribe \ "Event").text.equals("subscribe") =>
+          Ok(handleSubscribe(subscribe))
+        case clicking if (clicking \ "MsgType").text.equals("event") =>
+          Ok(handleClicking(clicking))
         case _ =>
           Ok("")
       }
