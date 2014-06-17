b24b413f2ad53393621d811e8a54524b29c17222
Suo Qin <kpsesuo@gmail.com>
2014-06-17 11:25:07 +0800

    allow delete and update device record at the backend

--- app/controllers/MacAddressController.scala
+++ app/controllers/MacAddressController.scala
@@ -1,12 +1,13 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
-import models.{Device, SuccessResponse}
+import models.{ErrorResponse, Device, SuccessResponse}
 import play.api.libs.json.{JsError, Json}
 
 object MacAddressController extends Controller with Secured {
 
   implicit val write = Json.writes[SuccessResponse]
+  implicit val write1 = Json.writes[ErrorResponse]
   implicit val write2 = Json.writes[Device]
   implicit val read = Json.reads[Device]
 
@@ -30,6 +31,26 @@ object MacAddressController extends Controller with Secured {
           Ok(Json.toJson(device.create()))
       }.recoverTotal {
         e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+      }
+  }
+
+  def update(notUsed: String) = OperatorPage(parse.json) {
+    u => request =>
+      request.body.validate[Device].map {
+        case (device: Device) =>
+          Ok(Json.toJson(device.update()))
+      }.recoverTotal {
+        e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+      }
+  }
+
+  def delete(id: Long) = OperatorPage {
+    u => _ =>
+      Device.delete(id) match {
+        case true =>
+          Ok(Json.toJson(new SuccessResponse))
+        case false =>
+          Ok(Json.toJson(new ErrorResponse("删除Mac失败")))
       }
   }
 
--- app/models/Device.scala
+++ app/models/Device.scala
@@ -6,7 +6,14 @@ import anorm.SqlParser._
 import play.api.Play.current
 import helper.MD5Helper.md5
 
-case class Device(mac: String, school_id: Long) {
+case class Device(id: Option[Long], mac: String, school_id: Long) {
+  def update() = DB.withConnection {
+    implicit c =>
+      SQL("update macwhitelist set school_id={kg}, mac={mac}, encoded_mac={encoded_mac}, update_at={time} where uid={id}")
+        .on('kg -> school_id.toString,'mac -> mac, 'encoded -> md5(mac), 'time -> System.currentTimeMillis, 'id -> id)
+        .executeUpdate()
+  }
+
   def create() = DB.withConnection {
     implicit c =>
       SQL("insert into macwhitelist (school_id, mac, encoded_mac, update_at) values ({kg}, {mac}, {encoded}, {time})")
@@ -16,11 +23,18 @@ case class Device(mac: String, school_id: Long) {
 }
 
 object Device {
+  def delete(id: Long) = DB.withConnection {
+    implicit c =>
+      SQL("delete from macwhitelist where uid={id}")
+        .on('id -> id).execute()
+  }
+
   val simple = {
+    get[Long]("uid") ~
     get[String]("school_id") ~
       get[String]("mac") map {
-      case kg ~ mac =>
-        Device(mac, kg.toLong)
+      case id ~ kg ~ mac =>
+        Device(Some(id), mac, kg.toLong)
     }
   }
 
