cce39ba7808a6fd89abc812f1ad9fdc8721c5a26
Suo Qin <kpsesuo@gmail.com>
2014-08-09 20:11:05 +0800

    add update and create method to video member

--- app/controllers/VideoMemberController.scala
+++ app/controllers/VideoMemberController.scala
@@ -1,7 +1,8 @@
 package controllers
 
-import models.VideoMember
-import play.api.libs.json.Json
+import controllers.SessionController._
+import models.{ErrorResponse, VideoMember}
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc.Controller
 
 object VideoMemberController extends Controller with Secured {
@@ -19,5 +20,38 @@ object VideoMemberController extends Controller with Secured {
         NotFound
     }
 
+  }
+
+  def create(kg: Long) = IsLoggedIn(parse.json) { u =>
+    implicit request =>
+      request.body.validate[VideoMember].map {
+        case (member) if !member.school_id.equals(Some(kg)) =>
+          BadRequest(Json.toJson(ErrorResponse("请提供一致的信息。")))
+        case (member) if member.isAccountDuplicated =>
+          BadRequest(Json.toJson(ErrorResponse(s"提供的账号${member.account.get}重复。")))
+        case (member) if member.isExisting =>
+          member.update
+          Ok(Json.toJson(VideoMember.show(kg, member.id)))
+        case (member) =>
+          member.create
+          Ok(Json.toJson(VideoMember.show(kg, member.id)))
+      }.recoverTotal {
+        e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+      }
+  }
+
+  def update(kg: Long, id: String) = IsLoggedIn(parse.json) { u =>
+    implicit request =>
+      request.body.validate[VideoMember].map {
+        case (member) if !member.school_id.equals(Some(kg)) || !id.equals(member.id) =>
+          BadRequest(Json.toJson(ErrorResponse("请提供一致的信息。")))
+        case (member) if member.isAccountDuplicated =>
+          BadRequest(Json.toJson(ErrorResponse(s"提供的账号${member.account.get}重复。")))
+        case (member) =>
+          member.update
+          Ok(Json.toJson(VideoMember.show(kg, member.id)))
+      }.recoverTotal {
+        e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+      }
   }
 }
--- app/models/VideoMember.scala
+++ app/models/VideoMember.scala
@@ -6,11 +6,48 @@ import play.api.Play.current
 import anorm.SqlParser._
 import anorm._
 import anorm.~
+import helper.MD5Helper.md5
 
-case class VideoMember(id: String, account: String, password: String, school_id: Option[Long])
+
+
+case class RawVideoMember(account: String)
+case class VideoMember(id: String, account: Option[String], password: Option[String], school_id: Option[Long]){
+  def update = DB.withConnection {
+    implicit c =>
+      SQL("update videomembers set account={account} where parent_id={id}")
+        .on('account -> generateAccount(account), 'id -> id ).executeUpdate()
+  }
+  def create = DB.withConnection {
+    implicit c =>
+      SQL("insert into videomembers (parent_id, school_id, account) " +
+        "values ({id}, {kg}, {account})")
+        .on('kg -> school_id, 'account -> generateAccount(account), 'id -> id ).executeInsert()
+  }
+
+  def isExisting = DB.withConnection {
+    implicit c =>
+      SQL("select count(1) from videomembers where school_id={kg} and parent_id={id}")
+        .on('kg -> school_id, 'id -> id ).as(get[Long]("count(1)") single) > 0
+  }
+
+  def isAccountDuplicated = DB.withConnection {
+    implicit c =>
+      SQL("select count(1) from videomembers where account={account}")
+        .on('account -> account ).as(get[Long]("count(1)") single) > 0
+  }
+
+  private def generateAccount(base: Option[String]) = {
+    base match {
+      case Some(key) => key
+      case None => md5(s"$school_id$id")
+    }
+  }
+
+}
 
 object VideoMember {
   implicit val write = Json.writes[VideoMember]
+  implicit val read = Json.reads[VideoMember]
 
   def all(kg: Long) = DB.withConnection {
     implicit c =>
@@ -28,7 +65,7 @@ object VideoMember {
     get[String]("parent_id") ~
     get[String]("account") map {
       case kg ~ id ~ account =>
-        VideoMember(id, account, "123", Some(kg.toLong))
+        VideoMember(id, Some(account), Some("123"), Some(kg.toLong))
     }
   }
 }
