d725ca93fce108e10a065ca7c898175f60507142
Suo Qin <kpsesuo@gmail.com>
2014-08-22 15:24:47 +0800

    add spec for class controller

---
+++ test/controllers/ClassControllerSpec.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import _root_.helper.TestSupport
+import models.ChildInfo
+import org.junit.runner.RunWith
+import org.specs2.mutable.Specification
+import org.specs2.runner.JUnitRunner
+import play.Logger
+import play.api.libs.json.{JsArray, JsValue, Json}
+import play.api.test.Helpers._
+import play.api.test.{FakeRequest, WithServer}
+
+@RunWith(classOf[JUnitRunner])
+class ClassControllerSpec extends Specification with TestSupport {
+
+  def loggedRequest(method: String, url: String) = {
+    FakeRequest(method, url).withSession("username" -> "13408654680", "token" -> "1386849160798")
+  }
+
+  "Class" should {
+    "check authentication first" in new WithServer {
+
+      val childResponse = route(FakeRequest(GET, "/kindergarten/93740362/class")).get
+
+      status(childResponse) must equalTo(UNAUTHORIZED)
+
+    }
+
+    "not be deleted while children belongs in it" in new WithServer {
+
+      val updateResponse = route(loggedRequest(DELETE, "/kindergarten/93740362/class/777888")).get
+
+      status(updateResponse) must equalTo(BAD_REQUEST)
+    }
+
+  }
+}
