a4bc6ddd38c0f54a2ea0890dd64bce7ae91a0c6d
Suo Qin <kpsesuo@gmail.com>
2015-04-01 20:53:06 +0800

    implement check in/out

--- app/controllers/V2/BusLocationController.scala
+++ app/controllers/V2/BusLocationController.scala
@@ -1,8 +1,8 @@
 package controllers.V2
 
 import controllers.Secured
-import models.{SuccessResponse, BusLocation}
-import play.api.libs.json.Json
+import models.{Advertisement, ErrorResponse, SuccessResponse, BusLocation}
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc.Controller
 
 object BusLocationController extends Controller with Secured  {
@@ -11,28 +11,46 @@ object BusLocationController extends Controller with Secured  {
       Ok(Json.toJson(BusLocation.index(kg, driverId)))
   }
 
-  def create(kg: Long, driverId: String) = IsLoggedIn {
-    u => _ =>
-      Ok(Json.toJson(new SuccessResponse))
+  def create(kg: Long, driverId: String) = IsLoggedIn(parse.json) {
+    u => request =>
+      request.body.validate[BusLocation].map {
+        case (location) =>
+          BusLocation.create(kg, driverId, location)
+          Ok(Json.toJson(new SuccessResponse))
+      }.recoverTotal {
+        e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+      }
+
   }
 
   def childOnBus(kg: Long, childId: String) = IsLoggedIn {
     u => _ =>
-      Ok(Json.toJson(BusLocation.child(kg, childId)))
+      BusLocation.child(kg, childId) match {
+        case Some(x) if x.status == 0 =>
+          NotFound(Json.toJson(ErrorResponse(s"今天${childId}已下车(Got off the school bus already today.)")))
+        case Some(x) =>
+          Ok(Json.toJson(x))
+        case None =>
+          NotFound(Json.toJson(ErrorResponse(s"今天没有${childId}的乘车记录(No riding records today.)")))
+      }
+      
   }
 
   def checkIn(kg: Long, driverId: String, childId: String) = IsLoggedIn {
     u => _ =>
+      BusLocation.checkIn(kg, driverId, childId)
       Ok(Json.toJson(new SuccessResponse))
   }
 
   def checkOut(kg: Long, driverId: String, childId: String) = IsLoggedIn {
     u => _ =>
+      BusLocation.checkOut(kg, driverId, childId)
       Ok(Json.toJson(new SuccessResponse))
   }
 
   def batchCheckOut(kg: Long, driverId: String) = IsLoggedIn {
     u => _ =>
+      BusLocation.BatchCheckOut(kg, driverId, List(""))
       Ok(Json.toJson(new SuccessResponse))
   }
 }
--- app/models/BusLocation.scala
+++ app/models/BusLocation.scala
@@ -1,14 +1,73 @@
 package models
 
+import org.joda.time.DateTime
 import play.api.libs.json.Json
+import play.api.db.DB
+import anorm._
+import play.api.Logger
+import anorm.SqlParser._
+import anorm.~
+import play.api.Play.current
 
-case class BusLocation(school_id: Long, driver_id: String, latitude: Double, longitude: Double, direction: Float, radius: Float, address: String, child_id: Option[String] = None, timestamp: Option[Long] = None)
+case class BusLocation(school_id: Long, driver_id: String, latitude: Double, longitude: Double, direction: Float,
+                       radius: Float, address: Option[String], child_id: Option[String] = None, timestamp: Option[Long] = None, status: Option[Int] = None)
 
 object BusLocation {
   implicit val busLocationReads = Json.reads[BusLocation]
   implicit val busLocationWrites = Json.writes[BusLocation]
 
-  def index(kg: Long, driverId: String) = List(BusLocation(kg, driverId, 108.883425, 34.253351, 0, 0, "", None, Some(System.currentTimeMillis)))
+  def simple(childId: Option[String]) = {
+    get[String]("school_id") ~
+      get[String]("employee_id") ~
+      get[Double]("latitude") ~
+      get[Double]("longitude") ~
+      get[Double]("direction") ~
+      get[Double]("radius") ~
+      get[Option[String]]("address") ~
+      get[Int]("status") ~
+      get[Long]("received_at") map {
+      case kg ~ driver ~ la ~ lg ~ dir ~ radius ~ address ~ status ~ time =>
+        BusLocation(kg.toLong, driver, la, lg, dir.toFloat, radius.toFloat, address, childId, Some(time), Some(status))
+    }
+  }
 
-  def child(kg: Long, childId: String) = List(BusLocation(kg, "", 108.883425, 34.253351, 0, 0, "", Some(childId), Some(System.currentTimeMillis)))
+  def index(kg: Long, driverId: String) = DB.withConnection {
+    implicit c =>
+      val beginOfDay: Long = DateTime.now().withTimeAtStartOfDay().getMillis
+      Logger.info(s"beginOfDay=$beginOfDay")
+      SQL("select * from buslocation where school_id={kg} and employee_id={id} and received_at > {day}").on('kg -> kg, 'id -> driverId, 'day -> beginOfDay).as(simple(None) *)
+  }
+
+  def child(kg: Long, childId: String) = DB.withConnection {
+    implicit c =>
+      val beginOfDay: Long = DateTime.now().withTimeAtStartOfDay().getMillis
+      Logger.info(s"beginOfDay=$beginOfDay")
+      SQL("select b.*, c.status from buslocation b, childrenonbus c where b.school_id={kg} and c.child_id={id} and b.school_id=c.school_id and b.employee_id=c.employee_id " +
+        "and b.received_at > {day} and c.received_at > {day} order by b.uid DESC limit 1")
+        .on('kg -> kg, 'id -> childId, 'day -> beginOfDay).as(simple(Some(childId)) singleOpt)
+  }
+
+  def create(kg: Long, employeeId: String, location: BusLocation) = DB.withConnection {
+    implicit c =>
+      SQL("insert into buslocation (school_id, employee_id, latitude, longitude, direction, radius, address, received_at) values " +
+        "({kg}, {driver}, {la}, {lg}, {dir}, {ra}, {address}, {time})")
+        .on('kg -> kg, 'driver -> employeeId, 'la -> location.latitude, 'lg -> location.longitude,
+          'dir -> location.direction, 'ra -> location.radius, 'address -> location.address, 'time -> System.currentTimeMillis).executeInsert()
+  }
+
+  def BatchCheckOut(kg: Long, employeeId: String, children: List[String]) = ???
+
+  def checkIn(kg: Long, employeeId: String, childId: String) = DB.withConnection {
+    implicit c =>
+      SQL("insert into childrenonbus (school_id, employee_id, child_id, card, received_at) values " +
+        "({kg}, {driver}, {child}, {card}, {time})")
+        .on('kg -> kg, 'driver -> employeeId, 'child -> childId, 'time -> System.currentTimeMillis, 'card -> "").executeInsert()
+  }
+
+  def checkOut(kg: Long, employeeId: String, childId: String) = DB.withConnection {
+    implicit c =>
+      SQL("update childrenonbus set status=0, received_at={time} where school_id={kg} and employee_id={driver} and child_id={child} ")
+        .on('kg -> kg, 'driver -> employeeId, 'child -> childId, 'time -> System.currentTimeMillis).executeInsert()
+  }
+
 }
