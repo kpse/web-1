e3b0df221b63b036c8c8ddb83d349b6712f2e1c5
Suo Qin <kpsesuo@gmail.com>
2015-05-01 20:34:26 +0800

    add v3 student interface

---
+++ app/controllers/V3/StudentController.scala
@@ -0,0 +1,90 @@
+package controllers.V3
+
+import controllers.Secured
+import models.{ChildInfo, SuccessResponse}
+import play.api.libs.json.{JsError, Json}
+import play.api.mvc.Controller
+import models.Children.readChildInfo
+import models.Children.writeChildInfo
+
+case class StudentExt(former_name: Option[String], student_id: Option[String], social_id: Option[String], Residence_place: Option[String],
+                      Residence_type: Option[String], nationality: Option[String], original_place: Option[String], ethnos: Option[String],
+                      student_type: Option[Int], InDate: Option[String], interest: Option[String], bed_number: Option[String], memo: Option[String],
+                      bus_status: Option[Int], medical_history: Option[String])
+
+case class Student(id: Option[Long], basic: ChildInfo, ext: StudentExt)
+
+object StudentController extends Controller with Secured {
+
+  implicit val writeStudentExt = Json.writes[StudentExt]
+  implicit val readStudentExt = Json.reads[StudentExt]
+
+  implicit val writeStudent = Json.writes[Student]
+  implicit val readStudent = Json.reads[Student]
+
+  def info(kg: Long): ChildInfo = ChildInfo(Some(s"1_${kg}_112121"), "小明", "嘟嘟", "2013-01-01", 1,
+    Some("http://suoqin-test.u.qiniudn.com/1D8530120BB9780D0A28F8283E117F13"), 1234, Some("苹果班"), Some(1427817610000L), Some(kg), Some("某个胡同"),
+    Some(1), Some(1417817610000L))
+
+  val ext: StudentExt = StudentExt(Some("曾用名"), Some("3721"), Some("510122201301010274"), Some("四川双流"), Some("农村"),
+    Some("中国"), Some("湖北武汉"), Some("傣族"), Some(1), Some("2015-09-01"), Some("骑马，打仗"), Some("1-2-3322"), Some("这家伙很懒，什么也没留下。"), Some(1), Some("青霉素过敏"))
+
+  def index(kg: Long) = IsLoggedIn { u => _ =>
+
+    Ok(Json.toJson(List(Student(Some(1), info(kg), ext))))
+  }
+
+  def show(kg: Long, id: Long) = IsLoggedIn { u => _ =>
+    Ok(Json.toJson(Student(Some(id), info(kg), ext)))
+  }
+
+  def create(kg: Long) = IsLoggedIn(parse.json) { u => request =>
+    request.body.validate[Student].map {
+      case (s) =>
+        Ok(Json.toJson(s))
+    }.recoverTotal {
+      e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+    }
+  }
+
+  def update(kg: Long, id: Long) = IsLoggedIn(parse.json) { u => request =>
+    request.body.validate[Student].map {
+      case (s) =>
+        Ok(Json.toJson(s))
+    }.recoverTotal {
+      e => BadRequest("Detected error:" + JsError.toFlatJson(e))
+    }
+  }
+
+  def delete(kg: Long, id: Long) = IsLoggedIn { u => _ =>
+    Ok(Json.toJson(new SuccessResponse()))
+  }
+}
+
+/*
+* create table IMSInfo.dbo.StudentInfo(
+SysNO int identity(1,1), --系统自增编号，1,2,3,4…..
+Name nvarchar(20), --姓名
+DisplayName nvarchar(20), --显示名称，一般跟姓名一样，遇到同名可靠这个区分，类似昵称
+FormerName nvarchar(20), --曾用名，一般不填
+Gender int default(0),性别，1男0女
+StudentID nvarchar(50),学号，可能有带字母的超长学号
+SocialID nvarchar(20),身份证号
+ResidencePlace nvarchar(50),户口所在地
+ResidenceType varchar(10),户口类型，enum（城镇户口，农村户口）
+Nationality nvarchar(20),国籍
+OriginPlace nvarchar(20),籍贯
+Ethnic nvarchar(20),民族
+ClassSysNO int,所属班级编号（班级表的SysNO）
+Birthday datetime,生日
+StudentType int,学生类型 1日托0全托
+Address nvarchar(200),家庭地址
+InDate datetime,入园日期
+Picture varchar(max),照片（base64  string直存数据库）
+Interest nvarchar(500),兴趣爱好
+BedNumber nvarchar(20),床号
+Memo nvarchar(500),备注
+BusStatus int是否乘坐校车,
+MedicalHistory nvarchar(2000) 病史
+)
+*/
