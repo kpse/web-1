c7f3dc59a2ecdda63d54b8565a1be5b4fbef64af
Suo Qin <kpsesuo@gmail.com>
2015-06-18 14:39:50 +0800

    fix deleting issue of health check

--- app/controllers/V3/MorningHealthCheckController.scala
+++ app/controllers/V3/MorningHealthCheckController.scala
@@ -94,7 +94,6 @@ object StudentMorningHealthCheckController extends Controller with Secured {
   }
 
   def delete(kg: Long, studentId: Long, id: Long) = IsLoggedIn { u => _ =>
-    HealthCheck.deleteById(kg, studentId, id)
     Ok(Json.toJson(new SuccessResponse()))
   }
 }
@@ -151,6 +150,7 @@ object StudentHealthCheckController extends Controller with Secured {
   }
 
   def delete(kg: Long, studentId: Long, id: Long) = IsLoggedIn { u => _ =>
+    HealthCheck.deleteById(kg, studentId, id)
     Ok(Json.toJson(new SuccessResponse()))
   }
 }
