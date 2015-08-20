ec86b51fee4eaeaf48b3aef7338546995935c39a
Suo Qin <kpsesuo@gmail.com>
2015-08-21 00:07:45 +0800

    fix session test

--- test/controllers/SessionControllerSpec.scala
+++ test/controllers/SessionControllerSpec.scala
@@ -50,7 +50,7 @@ class SessionControllerSpec extends Specification with TestSupport {
 
     "be able to delete by owner teacher" in new WithApplication {
 
-      val response = route(ownerTeacherRequest(DELETE, "/kindergarten/93740362/session/1_93740362_9982/record/39")).get
+      val response = route(ownerTeacherRequest(DELETE, "/kindergarten/93740362/session/1_93740362_9982/record/40")).get
 
       status(response) must equalTo(OK)
     }
