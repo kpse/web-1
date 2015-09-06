dee71320c7fc9468b9694265743f4d4b73b74826
Suo Qin <kpsesuo@gmail.com>
2015-09-06 20:32:44 +0800

    allow to update and insert new menu fields

--- app/models/V3/Menu.scala
+++ app/models/V3/Menu.scala
@@ -84,7 +84,7 @@ case class Menu(id: Option[Long], name: Option[String], weight: Option[String], 
   def update(kg: Long): Option[Menu] = DB.withTransaction {
     implicit c =>
       try {
-        SQL("update dietmenu set school_id={school_id}, name={name}, weight={weight}, " +
+        SQL("update dietmenu set school_id={school_id}, name={name}, weight={weight}, food_type_id={food_type_id}, former_id={former_id}, " +
           "arrange_type={arrange_type}, store_type={store_type}, updated_at={time} where school_id={school_id} and uid={id}")
           .on(
             'id -> id,
@@ -93,6 +93,8 @@ case class Menu(id: Option[Long], name: Option[String], weight: Option[String], 
             'weight -> weight,
             'arrange_type -> arrange_type,
             'store_type -> store_type,
+            'food_type_id -> food_type_id,
+            'former_id -> former_id,
             'time -> System.currentTimeMillis
           ).executeUpdate()
         id foreach {
@@ -118,14 +120,16 @@ case class Menu(id: Option[Long], name: Option[String], weight: Option[String], 
   def create(kg: Long): Option[Menu] = DB.withTransaction {
     implicit c =>
       try {
-        val insert: Option[Long] = SQL("insert into dietmenu (school_id, name, weight, arrange_type, updated_at) values (" +
-          "{school_id}, {name}, {weight}, {arrange_type}, {time})")
+        val insert: Option[Long] = SQL("insert into dietmenu (school_id, name, weight, arrange_type, food_type_id, former_id, updated_at) values (" +
+          "{school_id}, {name}, {weight}, {arrange_type}, {food_type_id}, {former_id}, {time})")
           .on(
             'school_id -> kg,
             'name -> name,
             'weight -> weight,
             'arrange_type -> arrange_type,
             'store_type -> store_type,
+            'food_type_id -> food_type_id,
+            'former_id -> former_id,
             'time -> System.currentTimeMillis
           ).executeInsert()
         insert foreach {
