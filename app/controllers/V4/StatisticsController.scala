a2171aae23fac1e2f56ea2eaae9117b47d5db87d
Suo Qin <kpsesuo@gmail.com>
2015-10-02 11:48:51 +0800

    add monthly school statistics interface

--- app/controllers/V4/StatisticsController.scala
+++ app/controllers/V4/StatisticsController.scala
@@ -1,5 +1,26 @@
 package controllers.V4
 
-object StatisticsController {
+import controllers.Secured
+import controllers.helper.CacheHelper._
+import models.V4.SchoolMonthlyStatistics
+import models.V4.SchoolMonthlyStatistics.writeSchoolMonthlyStatistics
+import org.joda.time.DateTime
+import play.api.libs.json.Json
+import play.api.mvc.Controller
+
+object StatisticsController extends Controller with Secured {
+
+  implicit val statisticsControllerCacheKey = "school_monthly_counting"
+  createKeyCache(statisticsControllerCacheKey)
+
+  def monthlyCounting(schoolId: Long, lastMonth: DateTime = DateTime.now().minusMonths(1)) = IsOperator {
+    u => _ =>
+      val month: String = SchoolMonthlyStatistics.pattern.print(lastMonth)
+      val cacheKey: String = s"monthlyCountFor${schoolId}_In_$month"
+      val value: SchoolMonthlyStatistics = digFromCache[SchoolMonthlyStatistics](cacheKey, 3600 * 24 * 20, () => {
+        SchoolMonthlyStatistics.collectTheWholeMonth(schoolId, lastMonth)
+      })
+      Ok(Json.toJson(value))
+  }
 
 }
