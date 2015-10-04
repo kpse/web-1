b9d08638a48daa888ee7180b6c67f0af1c91626a
Suo Qin <kpsesuo@gmail.com>
2015-10-04 12:07:20 +0800

    take advantage of travis cache

---
+++ travis_util.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+# check to see if protobuf folder is empty
+if [ ! -d "$HOME/play-${PLAY_VERSION}/play" ]; then
+  wget http://downloads.typesafe.com/play/${PLAY_VERSION}/play-${PLAY_VERSION}.zip
+  unzip -q play-${PLAY_VERSION}.zip
+  npm install
+else
+  echo 'Using cached directory.';
+fi
