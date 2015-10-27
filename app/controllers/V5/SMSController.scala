feb84b3812fe58198d14405fb78b6af420ba9bdf
Suo Qin <kpsesuo@gmail.com>
2015-10-27 14:51:38 +0800

    revise message content, retrieve parent info before sending message, hide unnecessary verify interface

--- app/controllers/V5/SMSController.scala
+++ app/controllers/V5/SMSController.scala
@@ -25,15 +25,21 @@ object SMSController extends Controller with Secured {
     implicit request =>
       implicit val provider: SMSProvider = SMSProvider.create
       floodProtect(phone) match {
-        case InvitationCode(_, _, time) if System.currentTimeMillis - time < 120*1000  =>
+        case InvitationCode(_, _, time, _) if System.currentTimeMillis - time < 120*1000  =>
           Future.successful(BadRequest(Json.toJson(ErrorResponse("请求太频繁，目前限制为2分钟。(too frequently requests, current is 2 minutes)"))))
-        case InvitationCode(p, c, t) =>
-          Logger.info(s"resend : ${InvitationCode(p, c, t)}")
-          sendSMS(InvitationCode(p, c, System.currentTimeMillis()).toPayload(phone))
+        case InvitationCode(p, c, t, parent) =>
+          Logger.info(s"resend : ${InvitationCode(p, c, t, parent)}")
+          sendSMS(InvitationCode(p, c, System.currentTimeMillis(), parent).toPayload())
         case null =>
-          val smsReq = InvitationCode.generate(phone)
-          Logger.info(smsReq.toString())
-          sendSMS(smsReq)
+          Parent.phoneSearch(phone) match {
+            case Some(parent) if parent.status == Some(1) =>
+              val smsReq = InvitationCode.generate(parent)
+              Logger.info(s"smsReq = $smsReq")
+              sendSMS(smsReq)
+            case _ =>
+              Future.successful(BadRequest(Json.toJson(ErrorResponse(s"未找到此手机号${phone}的注册信息。(unregistered phone number)", 6))))
+          }
+
         case _ =>
           Future.successful(BadRequest(Json.toJson(ErrorResponse("短信发送出错。(other errors in sending SMS, contact admin please)", 3))))
       }
--- app/models/V5/InvitationVerification.scala
+++ app/models/V5/InvitationVerification.scala
@@ -1,7 +1,7 @@
 package models.V5
 
 import models.CheatCode.validate
-import models.{SMSProvider, Verification}
+import models.{Parent, SMSProvider, Verification}
 import play.api.Logger
 import play.api.cache.Cache
 
@@ -12,17 +12,17 @@ case class InvitationPhoneKey(phone: String) {
   val iKey = s"${phone}_invitation_code"
 }
 
-case class InvitationCode(phone: String, code: String, created_at: Long) {
+case class InvitationCode(phone: String, code: String, created_at: Long, parent: Option[Parent] = None) {
   def isMatch(another: Option[InvitationCode]): Boolean = another.fold(false) { input => phone == input.phone && code == input.code }
-  def toPayload(phone: String)(implicit provider: SMSProvider) = {
+  def toPayload()(implicit provider: SMSProvider) = {
     val userName = provider.username()
     val secretKey = provider.password()
-    val pair = formatMessage(phone)(provider)
+    val pair = formatMessage()(provider)
     provider.render(userName, secretKey, phone, pair)
   }
 
-  def formatMessage(phone: String)(provider: SMSProvider): String = {
-    "验证码：%s，%s请您加入幼乐宝，共同关注%s的精彩瞬间。请将此验证码20分钟内转告给%s，欢迎搜索微信公众号“幼乐宝”。点击“软件下载”安装使用。".format(code, "老王", "小王", "老王")
+  def formatMessage()(implicit provider: SMSProvider): String = {
+    s"验证码：${code}，${parent.get.name}请您加入幼乐宝，共同关注孩子的精彩瞬间。请将此验证码20分钟内转告给${parent.get.name}，欢迎搜索微信公众号“幼乐宝”。点击“软件下载”安装使用。"
   }
 }
 
@@ -36,17 +36,17 @@ object InvitationCode {
     verification.toCodeVerification.isMatch(Cache.getAs[InvitationCode](verification.phone.iKey)) || validate(verification.code)
   }
 
-  def newCodeFromPhoneNumber(phone: String) = {
+  def newCodeFromPhoneNumber(parent: Parent) = {
     val random = new Random(System.currentTimeMillis)
     val code = "%06d".format(random.nextInt(999999))
-    logger.info(phone + "'s code : " + code)
-    val invitationCode: InvitationCode = InvitationCode(phone, code, System.currentTimeMillis)
-    Cache.set(phone.iKey, invitationCode, 12 * 60)
+    logger.info(parent.phone + "'s code : " + code)
+    val invitationCode: InvitationCode = InvitationCode(parent.phone, code, System.currentTimeMillis, Some(parent))
+    Cache.set(parent.phone.iKey, invitationCode, 12 * 60)
     invitationCode
   }
 
-  def generate(phone: String)(implicit provider: SMSProvider) = {
-    val code = newCodeFromPhoneNumber(phone)
-    code.toPayload(phone)(provider)
+  def generate(parent: Parent)(implicit provider: SMSProvider) = {
+    val code = newCodeFromPhoneNumber(parent)
+    code.toPayload()(provider)
   }
 }
