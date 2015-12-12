258b5129163a75fcd64e7e33a8228f3d83128233
Suo Qin <kpsesuo@gmail.com>
2015-12-12 21:31:08 +0800

    add create testing to stocking spec

--- test/models/V3/StockingSpec.scala
+++ test/models/V3/StockingSpec.scala
@@ -16,10 +16,27 @@ class StockingSpec extends Specification with TestSupport {
 
       val updated: Stocking = newStocking.update(93740362, 1).get
 
-      updated.items.get.size must equalTo(exists.head.items.get.size + 1)
+      updated.items.get.size must equalTo(newStocking.items.get.size)
 
       val DataFromDb: Stocking = Stocking.show(93740362, 1, updated.id.get).get
       DataFromDb.items.get.size must equalTo(updated.items.get.size)
+      DataFromDb.items.get.last.employee_id must beSome(employeeId)
+      DataFromDb.items.get.last.employee_name must beSome(employeeName)
+    }
+
+    "add items with employee info in creating" in new WithApplication {
+      val employeeId: Long = 998
+      val employeeName: String = "employeeName"
+
+      val exists: List[Stocking] = Stocking.index(93740362, 1, None, None, Some(1))
+      val newStocking: Stocking = exists.head.copy(id = None, items = Some(exists.head.items.get ::: List(StockingDetail(None, exists.head.id, Some(1), None, Some(employeeId), Some(employeeName), None, None, None, None, None, None, None))))
+
+      val created: Stocking = newStocking.create(93740362, 1).get
+
+      created.items.get.size must equalTo(newStocking.items.get.size)
+
+      val DataFromDb: Stocking = Stocking.show(93740362, 1, created.id.get).get
+      DataFromDb.items.get.size must equalTo(created.items.get.size)
       DataFromDb.items.get.last.employee_id must beSome(employeeId)
       DataFromDb.items.get.last.employee_name must beSome(employeeName)
     }
