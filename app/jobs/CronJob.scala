73e0a647f1f37e2d9839c642b52d6e0bfe4ad56e
Suo Qin <kpsesuo@gmail.com>
2016-01-01 14:51:16 +0800

    add monthly and daily school data to cron job

--- app/jobs/CronJob.scala
+++ app/jobs/CronJob.scala
@@ -2,7 +2,7 @@ package jobs
 
 import akka.actor.Actor
 import models.School
-import models.V4.{AgentSchool, AgentStatistics, KulebaoAgent}
+import models.V4.{SchoolOperationReport, AgentSchool, AgentStatistics, KulebaoAgent}
 import models.json_models.SchoolIntro
 import org.joda.time.DateTime
 import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
@@ -15,10 +15,18 @@ class CronJob extends Actor {
   private val log = Logger(classOf[CronJob])
 
   def receive = {
-    case Tick if DateTime.now().dayOfMonth().get() == 1 => {
-      log.info("Got tick at the first day of the month")
-
+    case Tick if DateTime.now().dayOfMonth().get() == 1 && DateTime.now().hourOfDay().get < 2 => {
+      log.info(s"Got tick at the first day of the month, do Agent stats at ${DateTime.now()}")
       KulebaoAgent.monthlyStatistics
+    }
+    case Tick if DateTime.now().dayOfMonth().get() == 1 && DateTime.now().hourOfDay().get < 5 => {
+      log.info(s"Got tick at the first day of the month, do School stats at ${DateTime.now()}")
+      SchoolOperationReport.monthlyStatistics
+    }
+    case Tick if DateTime.now().hourOfDay().get() < 5 => {
+      log.info(s"Got tick at the first hours of the day ${DateTime.now().hourOfDay().get()}")
+
+      SchoolOperationReport.dailyStatistics
     }
     case Tick =>
       log.info("Got a normal tick")
