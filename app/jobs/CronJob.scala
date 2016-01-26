b58d669c807f275701fc7b0dae944cb6aa8d5c93
Suo Qin <kpsesuo@gmail.com>
2016-01-26 21:39:28 +0800

    move week condition above daily cronjob

--- app/jobs/CronJob.scala
+++ app/jobs/CronJob.scala
@@ -16,17 +16,17 @@ class CronJob extends Actor {
       log.info(s"Got tick at the first day of the month, do Agent stats at ${DateTime.now()}")
       KulebaoAgent.monthlyStatistics
     }
-    case Tick if DateTime.now().dayOfMonth().get() == 1 && DateTime.now().hourOfDay().get < 5 => {
+    case Tick if DateTime.now().dayOfMonth().get() == 1 && DateTime.now().hourOfDay().get < 3 => {
       log.info(s"Got tick at the first day of the month, do School stats at ${DateTime.now()}")
       SchoolOperationReport.monthlyStatistics
-    }
-    case Tick if DateTime.now().hourOfDay().get() < 5 => {
-      log.info(s"Got tick at the first hours of the day ${DateTime.now().hourOfDay().get()}")
-      SchoolOperationReport.dailyStatistics
     }
     case Tick if DateTime.now().dayOfWeek().get() == DateTimeConstants.MONDAY && DateTime.now().hourOfDay().get() < 4 => {
       log.info(s"Got tick at the first day of the week ${DateTime.now().weekOfWeekyear().get()}")
       SchoolWeeklyReport.weeklyStatistics
+    }
+    case Tick if DateTime.now().hourOfDay().get() < 5 => {
+      log.info(s"Got tick at the first hours of the day ${DateTime.now().hourOfDay().get()}")
+      SchoolOperationReport.dailyStatistics
     }
     case Tick =>
       log.info("Got a normal tick, do nothing")
