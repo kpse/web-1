8405211d77d774ce82b33789e21704487dc137e5
Suo Qin <kpsesuo@gmail.com>
2016-03-29 23:58:18 +0800

    add new home page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -22,6 +22,10 @@ object Application extends Controller with Secured {
     Ok(views.html.index())
   }
 
+  def newHome = Action {
+    Ok(views.html.newHome())
+  }
+
   def admin = IsAuthenticated {
     username =>
       _ =>
