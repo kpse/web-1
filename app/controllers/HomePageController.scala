d511cfa836ffe63047e504311f763828827d54ff
Suo Qin <kpsesuo@gmail.com>
2016-04-08 18:07:13 +0800

    add under construction page

--- app/controllers/HomePageController.scala
+++ app/controllers/HomePageController.scala
@@ -9,7 +9,8 @@ object HomePageController extends Controller with Secured {
   }
 
   def software = Action {
-    Ok(views.html.v2.software())
+//    Ok(views.html.v2.software())
+    Redirect("/constructing")
   }
 
   def hardware = Action {
@@ -17,15 +18,22 @@ object HomePageController extends Controller with Secured {
   }
 
   def becomeUser = Action {
-    Ok(views.html.v2.becomeUser())
+//    Ok(views.html.v2.becomeUser())
+    Redirect("/constructing")
   }
 
   def becomeAgent = Action {
-    Ok(views.html.v2.becomeAgent())
+//    Ok(views.html.v2.becomeAgent())
+    Redirect("/constructing")
   }
 
   def aboutUs = Action {
-    Ok(views.html.v2.aboutUs())
+//    Ok(views.html.v2.aboutUs())
+    Redirect("/constructing")
+  }
+
+  def underConstruction = Action {
+    Ok(views.html.v2.underConstruction())
   }
 
 }
