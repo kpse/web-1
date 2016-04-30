7743da02956b5f483bea0a0f831ec687948f54df
Suo Qin <kpsesuo@gmail.com>
2016-04-30 20:03:53 +0800

    move school config spec out of v2

--- test/models/SchoolConfigSpec.scala
+++ test/models/SchoolConfigSpec.scala
@@ -6,7 +6,7 @@ import org.specs2.mutable.Specification
 class SchoolConfigSpec extends Specification with TestSupport {
 
   "SchoolConfig" should {
-    "should apply default value to each config item" in {
+    "apply default value to each config item" in {
 
       val allDefaults: List[ConfigItem] = SchoolConfig.appendDefaultValue(List())
 
@@ -14,12 +14,25 @@ class SchoolConfigSpec extends Specification with TestSupport {
       allDefaults must contain(ConfigItem("schoolGroupChat", "true"))
     }
 
-    "should be able to override default value" in {
+    "be able to override default value" in {
 
       val allDefaults: List[ConfigItem] = SchoolConfig.appendDefaultValue(List(ConfigItem("displayVideoMemberDetail", "true")))
 
       allDefaults must contain(ConfigItem("displayVideoMemberDetail", "true"))
       allDefaults must contain(ConfigItem("schoolGroupChat", "true"))
+    }
+
+    "search both global and individual keys for value" in new WithApplication {
+
+      private val value: Option[String] = SchoolConfig.valueOfKey(93740562, "aa")
+
+      value must beNone
+
+      SchoolConfig.addConfig(93740562, ConfigItem("aa", "vv", SchoolConfig.SCHOOL_INDIVIDUAL_SETTING))
+
+      private val value2: Option[String] = SchoolConfig.valueOfKey(93740562, "aa")
+
+      value2 must beSome("vv")
     }
 
   }
