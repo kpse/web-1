003d70fcafa84a8dc8195cb2c6003a94b84ed8f1
Suo Qin <kpsesuo@gmail.com>
2016-05-19 17:49:42 +0800

    count real consumers number by parent ext

--- app/models/V2/SchoolSms.scala
+++ app/models/V2/SchoolSms.scala
@@ -1,11 +1,15 @@
 package models.V2
 
+import anorm.SqlParser._
+import anorm._
 import models.SchoolConfig
+import play.api.db.DB
 import play.api.libs.json.Json
 import play.api.libs.ws.WS
 
-import scala.concurrent.{Future,ExecutionContext}
+import scala.concurrent.{ExecutionContext, Future}
 import ExecutionContext.Implicits.global
+import play.api.Play.current
 
 case class SchoolSms(school_id: Long, available: Long, consumers: Long, pagination: Long = 65)
 
@@ -18,12 +22,18 @@ object SchoolSms {
     val password = SchoolConfig.valueOfKey(schoolId, "smsPushPassword").getOrElse("")
     WS.url(s"http://mb345.com:999/ws/SelSum.aspx?CorpID=${account}&Pwd=${password}").get() map {
       case (res) if res.body.toInt > 0 =>
-        SchoolSms(schoolId, res.body.toInt, allFeaturePhones)
+        SchoolSms(schoolId, res.body.toInt, allFeaturePhones(schoolId))
       case _ =>
-        SchoolSms(schoolId, 0, allFeaturePhones)
+        SchoolSms(schoolId, 0, allFeaturePhones(schoolId))
     }
   }
 
-  def allFeaturePhones = 120
+  def allFeaturePhones(schoolId: Long) = DB.withConnection {
+    implicit c =>
+      SQL("select count(1) from parentinfo p, parentext ex where p.uid = ex.base_id and school_id={kg} and p.status=1 and sms_push=1")
+        .on(
+          'kg -> schoolId.toString
+        ).as(get[Long]("count(1)") single)
+  }
 
 }
